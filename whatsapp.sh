a0e160bfc426252c88095d399e97f57e6aa1dbc0
vednoc <vednoc@protonmail.com>
2020-08-21 23:32:09 +0200

    build: print error and exit on invalid arguments

--- whatsapp.sh
+++ whatsapp.sh
@@ -3,6 +3,11 @@
 # Basic tool to convert `wa.user.css` to `darkmode.css` to
 # be then used with Ferdi/Franz/etc.
 
+[ $# -eq 0 ] && {
+    echo "Invalid arguments. Usage: $0 -h" >&2
+    exit 1
+}
+
 echo 'Converting...'
 
 input="wa.user.css"
